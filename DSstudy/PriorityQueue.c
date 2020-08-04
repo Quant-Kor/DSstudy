7bb940251a1c22c1ebef5a074d3ce2392609e7fe
Quant-Kor <53992007+Quant-Kor@users.noreply.github.com>
2020-08-04 15:02:04 +0900

    Priority Queue

---
+++ DSstudy/PriorityQueue.c
@@ -0,0 +1,20 @@
+#include "PriorityQueue.h"
+#include "UsefulHeap.h"
+
+void PQueueInit(PQueue* ppq, PriorityComp pc)
+{
+	HeapInit(ppq, pc);
+}
+int PQIsEmpty(PQueue* ppq)
+{
+	HIsEmpty(ppq);
+}
+
+void PEnqueue(PQueue* ppq, PQData data)
+{
+	HInsert(ppq, data);
+}
+PQData PDequeue(PQueue* ppq)
+{
+	return HDelete(ppq);
+}
--- DSstudy/UsefulHeap.h
+++ DSstudy/UsefulHeap.h
@@ -7,7 +7,7 @@
 /*** Heap¿« ¡§¿« ****/
 #define HEAP_LEN	100
 
-typedef char HData;
+typedef char * HData;
 typedef int PriorityComp(HData d1, HData d2);
 
 typedef struct _heap
