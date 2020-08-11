e59bce0d61ad8b8e84b73115536fc8779df0d2c6
Quant-Kor <53992007+Quant-Kor@users.noreply.github.com>
2020-08-11 16:03:20 +0900

    RadixSortf

--- DSstudy/RadixSort.c
+++ DSstudy/RadixSort.c
@@ -5,7 +5,7 @@
 
 void RadixSort(int arr[], int num, int maxLen)
 {
-	// 매개변수 maxLen에는 정렬 대상 중 가장 긴 데이터의 길이 정보가 전달
+	// 매개변수 maxLen에는 정렬대상 중 가장 긴 데이터의 길이 정보가 전달
 	Queue buckets[BUCKET_NUM];
 	int bi;
 	int pos;
@@ -17,20 +17,23 @@ void RadixSort(int arr[], int num, int maxLen)
 	for (bi = 0; bi < BUCKET_NUM; bi++)
 		QueueInit(&buckets[bi]);
 
-	// 가장 긴 데이터의 길이 만큼 반복
+	// 가장 긴 데이터의 길이만큼 반복
 	for (pos = 0; pos < maxLen; pos++)
 	{
+		// 정렬 대상의 수 만큼 반복
 		for (di = 0; di < num; di++)
 		{
-			//N번째 자리의 숫자 추출
+			// N번째 자리의 숫자 추출
 			radix = (arr[di] / divfac) % 10;
 
 			// 추출한 숫자를 근거로 버킷에 데이터 저장
 			Enqueue(&buckets[radix], arr[di]);
 		}
 
+		// 버킷 수 만큼 반복
 		for (bi = 0, di = 0; bi < BUCKET_NUM; bi++)
 		{
+			// 버킷에 저장된 순서대로 다 꺼내서 다시 arr에 저장
 			while (!QIsEmpty(&buckets[bi]))
 				arr[di++] = Dequeue(&buckets[bi]);
 		}
@@ -42,16 +45,16 @@ void RadixSort(int arr[], int num, int maxLen)
 
 int main(void)
 {
-	int arr[7] = { 13, 212, 14, 7141, 10987, 6, 15 };
+	int arr[7] = { 13, 212 ,14,7141,10987,6,15 };
 
 	int len = sizeof(arr) / sizeof(int);
 	int i;
-	
+
 	RadixSort(arr, len, 5);
 
 	for (i = 0; i < len; i++)
 		printf("%d ", arr[i]);
-	
+
 	printf("\n");
 	return 0;
 }
